43774df47c66fc3f7c4620eeb69858ea3159d5fa
akhzarna <link2akhzar@gmail.com>
2024-02-15 12:13:23 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{fontSize:56}}>  Bismillah In The name of Allah The most The Most </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
